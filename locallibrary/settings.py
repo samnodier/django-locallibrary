f77b732b0e860f3ad76c0a350ca33e9add629fe0
proprgrmmr <61632722+sam0132nodier@users.noreply.github.com>
2020-09-25 03:38:39 +0200

    Add files and changes to commit to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 import os
 # import django_heroku
 
@@ -128,8 +128,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Heroku update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # The absolute path to the directory where collectstatic will collect static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
