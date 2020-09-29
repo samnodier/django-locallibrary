220b85e5e2c020c44c12db3030c2aacba9510875
proprgrmmr <61632722+sam0132nodier@users.noreply.github.com>
2020-09-29 07:00:35 +0200

    Add files and changes required for heroku hosting

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,5 +157,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Reducing the size of the static files
 # Simplified static file serving
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # django_heroku.settings(locals())
