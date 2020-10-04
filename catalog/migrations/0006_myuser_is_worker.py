89e0d9507051a5386f2a4bc224dc1ac1c5c4a6a5
proprgrmmr <61632722+sam0132nodier@users.noreply.github.com>
2020-10-04 03:34:51 +0200

    Add files and changes to commit to heroku

--- catalog/migrations/0006_myuser_is_worker.py
+++ catalog/migrations/0006_myuser_is_worker.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='myuser',
+            model_name='MyUser',
             name='is_worker',
             field=models.BooleanField(default=False),
         ),
